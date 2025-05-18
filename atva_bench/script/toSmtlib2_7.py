2b79b5682c2627b5297cbff86ee736ba4c4d3d6b
SimpleXiaohu <1048448520@qq.com>
2025-05-18 21:04:36 +0800

    add more translation

--- atva_bench/script/toSmtlib2_7.py
+++ atva_bench/script/toSmtlib2_7.py
@@ -1,17 +1,12 @@
 ##=== (seq.write seq i str) write a string str to the i-th position of a sequence seq and return the updated sequence
-# (seq.write (Seq String) Int String (Seq String))
 
 ##=== (seq.filter seq regex) returns a sequence of all elements in seq that match the regex
-# (seq.filter (Seq String) RegLan (Seq String))
 
 ##=== (seq.join seq sep) returns a string that is the result of joining all elements in seq with the separator sep
-# (seq.join (Seq String) String)
 
 # (str.split str sep) returns a sequence of strings obtained by splitting str using the separator sep
-# (str.split String (Seq String))
 
 ##=== (str.match_all str regex) returns a sequence of all matches of regex in str
-# (str.match_all String RegLan (Seq String))
 
 
 ###--- For sequence operations and the extend operations above, I want to use string operations to encode them.
@@ -20,32 +15,44 @@
 ##=== (declare-const seq (Seq String))
 # (declare-const seq String)
 # (define-fun is_seq_re () RegLan (re.* (re.++ (str.to_re "#") re.all)))
-# (assert (str.in_re is_seq_re))
+# (assert (str.in_re seq is_seq_re))
+
+##=== (declare-const str String)
+# (define-fun is_str_re () RegLan (re.comp is_seq_re)
+# (assert (str.in_re str is_str_re))
 
 ##=== (seq.++ (seq1 seq2))
 # (str.++ seq1 seq2)
 
 ##=== (seq.len seq)
-# (declare-const seq_len_help String)
-# (assert (= seq_len_help (str.replace_all seq (re.comp (is_seq_re) ""))))
-# (define-fun seq_len ((seq String)) (str.len seq_len_help))
+# (define-fun seq_len ((seq String)) Int
+#   (let ((seq_len_help (str.replace_re_all seq is_str_re "")))
+#     (str.len seq_len_help)
+#   )
+# )
 
 ##=== (seq.at seq offset)
-# (declare-const seq_at_help_before String)
-# (declare-const seq_at_help_after String)
-# (declare-const seq_at_help_offset String)
-
-# (assert (= seq (str.++ seq_at_help_before seq_at_help_offset seq_at_help_after)))
-
-# (assert (str.in_re seq_at_help_before is_seq_re))
-# (assert (str.in_re seq_at_help_after is_seq_re))
-# (assert (str.in_re seq_at_help_offset is_seq_re))
-# (assert (= (seq_len seq_at_help_before) offset))
-# (define-fun seq_at ((seq String) (offset Int)) seq_at_help_offset)
+# (declare-fun seq_at_before (String Int) String)
+# (declare-fun seq_at_in (String Int) String)
+# (declare-fun seq_at_after (String Int) String)
+# (define-fun seq_at ((seq String) (i Int)) String
+#   (ite 
+#     (and (str.in_re (seq_at_before seq i) is_seq_re) (str.in_re (seq_at_in seq i) is_seq_re) (str.in_re (seq_at_after seq i) is_seq_re))
+#     (ite 
+#       (and (= i (seq_len (seq_at_before seq i))) (= seq (str.++ (seq_at_before seq i) (seq_at_in seq i) (seq_at_after seq i))))
+#       (seq_at_in seq i)
+#       ""
+#     )
+#     ""
+#   )
+# )
 
 ##=== (seq.nth seq offset)
-# (declare-const seq_nth_help)
-# (assert (= (str.++ "#" seq_nth_help) (seq_at seq offset)))
+# (define-fun seq_nth ((seq String) (i Int)) String
+#   (let ((seq_nth_help (seq_at seq i)))
+#     (str.substr seq_nth_help 1 (str.len seq_nth_help))
+#   )
+# )
 
 ##=== (seq.write seq i str)
 # (declare-const seq_write_help_before String)
@@ -60,18 +67,120 @@
 # (assert (= (seq_len seq_write_help_before) offset))
 # (define-fun seq_write ((seq String) (offset Int) (str String)) (str.++ seq_write_help_before str seq_write_help_after))
 
-##=== (seq.filter seq regex) 
-##=== this is a transducer indeed, we can only apply the filter to a fixed times, which is 3 here
-
-
-
 ##=== (str.split str sep)
 # (define-fun str_split ((str String) (sep String)) 
 #   (str.++ "#" (str.replace_all str sep "#"))
 # )
 
 ##=== (str.join str sep)
-# (declare-const str_join_help String)
-# (assert (= str (str.++ "#" str_join_help)))
+# (define-fun seq_join ((seq String) (sep String)) String 
+#   (let 
+#     ((join_help (str.substr seq 1 (str.len seq))))   ; remove the first sequence splitter
+#     (str.replace_all join_help "#" sep)
+#   )
+# )
+
+##=== the seq.filter and str.match_all can not be encoded by string operations so we do not translate them on purpose to make solvers return unknown
+
+import re, argparse
+
+def translateStrDeclare(smtStr: str) -> str :
+  strDeclarRE = r'\(declare-const\s+(\w+)\s+String\)'
+  return re.sub(strDeclarRE, r'(declare-const \1 String)\n(assert (str.in_re \1 is_str_re))', smtStr)
+
+def translateSeqDeclare(smtStr: str) -> str :
+  seqDeclareRE = r'\(declare-const\s+(\w+)\s+\(Seq\s+String\)\)'
+  return re.sub(seqDeclareRE, r'(declare-const \1 String)\n(assert (str.in_re \1 is_seq_re))', smtStr)
+
+def translateSeqUnit(smtStr: str) -> str :
+  seqUnitRE = r'\(seq\.unit\s+\"(\w+)\"\)'
+  return re.sub(seqUnitRE, r'(str.++ seq_spliter "\1")', smtStr)
+
+def translateSeqWrite(smtStr: str) -> str :
+  seqWriteRE = r'seq\.write'
+  return re.sub(seqWriteRE, r'seq_write', smtStr)
+
+def translateSeqNth(smtStr: str) -> str :
+  seqNthRE = r'seq\.nth'
+  return re.sub(seqNthRE, r'seq_nth', smtStr)
+
+def translateSeqAt(smtStr: str) -> str :
+  seqAtRE = r'seq\.at'
+  return re.sub(seqAtRE, r'seq_at', smtStr)
+
+def translateSeqLen(smtStr: str) -> str :
+  seqLenRE = r'seq\.len'
+  return re.sub(seqLenRE, r'seq_len', smtStr)
+
+def translateSeqJoin(smtStr: str) -> str :
+  seqJoinRE = r'seq\.join'
+  return re.sub(seqJoinRE, r'seq_join', smtStr)
+
+def translateStrSplit(smtStr: str) -> str :
+  strSplitRE = r'str\.split'
+  return re.sub(strSplitRE, r'str_split', smtStr)
+
+def translateSeqConcat(smtStr: str) -> str :
+  seqConcatRE = r'seq\.\+\+'
+  return re.sub(seqConcatRE, r'str.++', smtStr)
+
+def removeSetLogic(smtStr: str) -> str :
+  setLogicRE = r'\(set-logic\s+\w+\)'
+  return re.sub(setLogicRE, '', smtStr)
+
+def addSetLogicHeader(smtStr: str) -> str :
+  setLogic = '(set-logic QF_ALL)\n'
+  return setLogic + smtStr
+
+def addStrSpliterHeader(smtStr: str) -> str :
+  strSpliter = '(define-fun seq_spliter () String "#")\n'
+  return strSpliter + smtStr
+
+def addSeqRegexHeader(smtStr: str) -> str :
+  seqRegex = '(define-fun is_seq_re () RegLan (re.* (re.++ (str.to_re seq_spliter) re.all)))\n'
+  return seqRegex + smtStr
+
+def addStrRegexHeader(smtStr: str) -> str :
+  strRegex = '(define-fun is_str_re () RegLan (re.comp is_seq_re))\n'
+  return strRegex + smtStr
+
+def addSeqLenHeader(smtStr: str) -> str :
+  seqLen = (
+    '(define-fun seq_len ((seq String)) Int\n',
+    '  (let\n',
+    '    ((seq_len_help (str.replace_re_all seq is_str_re "")))\n',
+    '    (str.len seq_len_help)\n',
+    '  )\n',
+    ')\n'
+  )
+  return ''.join(seqLen) + smtStr
+  
+    
+
+parser = argparse.ArgumentParser(description="Translate sequence operations to string operations")
+parser.add_argument("filename", type=str, help="The input file name")
+args = parser.parse_args()
+
+def translate(file: str):
+  with open(file, 'r') as f:
+    smtStr = f.read()
+
+  # Translate sequence operations to string operations
+  smtStr = translateStrDeclare(smtStr) 
+  smtStr = translateSeqDeclare(smtStr)
+  smtStr = translateSeqUnit(smtStr)
+  smtStr = translateSeqWrite(smtStr)
+  smtStr = translateSeqNth(smtStr)
+  smtStr = translateSeqConcat(smtStr)
+  smtStr = translateSeqAt(smtStr)
+  smtStr = translateSeqLen(smtStr)
+  smtStr = translateSeqJoin(smtStr)
+  smtStr = translateStrSplit(smtStr)
+  smtStr = removeSetLogic(smtStr)
+  smtStr = addSeqLenHeader(smtStr)
+  print(smtStr)
+
+translate(args.filename)
+
 
 
